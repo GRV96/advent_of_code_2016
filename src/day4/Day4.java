3ba2a3120c51a17c902a04b574c9036d71c7b6e8
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2024-07-23 14:30:10 -0400

    Puzzle 2: 984 (northpole object storage)

--- src/day4/Day4.java
+++ src/day4/Day4.java
@@ -5,13 +5,21 @@ import src.day4.char_count.CharacterCounter;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Day4
 {
     private static final char DASH = '-';
     private static final char OPENING_BRACKET = '[';
+    private static final char SPACE = ' ';
 
     private static final int CHECKSUM_LENGTH = 5;
+
+    private static final String EMPTY_STR = "";
+    private static final String NORTH = "north";
+    private static final String NEW_LINE_TAB = "\n\t";
+
 
     private static boolean isRoomCodeLegit(RoomCode pRoomCode)
     {
@@ -40,6 +48,8 @@ public class Day4
         BufferedReader reader = new BufferedReader(new FileReader(inputPath));
 
         int sectorIdSum = 0;
+        CeasarDecrypter decrypter = new CeasarDecrypter();
+        List<String> suspectNames = new ArrayList<>();
         String inputLine;
         while ((inputLine = reader.readLine()) != null)
         {
@@ -49,9 +59,28 @@ public class Day4
             {
                 sectorIdSum += roomCode.sectorId;
             }
+
+            String decryptedName = decrypter.decryptMessage(
+                    roomCode.name.replace(DASH, SPACE), roomCode.sectorId);
+
+            if (decryptedName.contains(NORTH))
+            {
+                suspectNames.add(roomCode.sectorId + " (" + decryptedName + ")");
+            }
         }
 
         System.out.println("Puzzle 1: " + sectorIdSum);
+        System.out.println("Puzzle 2: " + makePuzzle2Output(suspectNames));
+    }
+
+    private static String makePuzzle2Output(List<String> pSuspectNames)
+    {
+        return switch (pSuspectNames.size())
+        {
+            case 0 -> EMPTY_STR;
+            case 1 -> pSuspectNames.getFirst();
+            default -> NEW_LINE_TAB + String.join(NEW_LINE_TAB, pSuspectNames);
+        };
     }
 
     private static RoomCode makeRoomCode(String pPuzzleLine)
