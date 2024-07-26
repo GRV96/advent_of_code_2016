3db8b34b2b9922e7329d61c75ae81b6d79b7f806
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2024-07-26 00:18:09 -0400

    Puzzle 1: 286 (wrong)

---
+++ src/day8/Day8.java
@@ -0,0 +1,59 @@
+package src.day8;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+class Day8
+{
+    private static final String CMD_RECT = "rect";
+    private static final String CMD_ROTATE = "rotate";
+
+    private static final String COLUMN = "column";
+    private static final String ROW = "row";
+
+    private static final String SPACE = " ";
+    private static final String X = "x";
+
+    public static void main(String[] pArgs) throws IOException
+    {
+        String inputPath = pArgs[0];
+        BufferedReader reader = new BufferedReader(new FileReader(inputPath));
+
+        Screen screen = new Screen(50, 6);
+        String inputLine;
+        while ((inputLine = reader.readLine()) != null)
+        {
+            inputLine = inputLine.trim();
+
+            String[] splitCommand = inputLine.split(SPACE);
+            switch (splitCommand[0])
+            {
+                case CMD_RECT -> parseRectCommand(screen, splitCommand);
+                case CMD_ROTATE -> parseRotateCommand(screen, splitCommand);
+            }
+        }
+
+        System.out.println("Puzzle 1: " + screen.countPixelsOn());
+    }
+
+    private static void parseRectCommand(Screen pScreen, String[] pSplitCommand)
+    {
+        String[] splitDimensions = pSplitCommand[1].split(X);
+        int nbColumns = Integer.parseInt(splitDimensions[0]);
+        int nbRows = Integer.parseInt(splitDimensions[1]);
+        pScreen.turnRectangleOn(nbColumns, nbRows);
+    }
+
+    private static void parseRotateCommand(Screen pScreen, String[] pSplitCommand)
+    {
+        int index = Integer.parseInt(pSplitCommand[2].substring(2));
+        int shift = Integer.parseInt(pSplitCommand[4]);
+
+        switch (pSplitCommand[1])
+        {
+            case COLUMN -> pScreen.rotateColumn(index, shift);
+            case ROW -> pScreen.rotateRow(index, shift);
+        }
+    }
+}
