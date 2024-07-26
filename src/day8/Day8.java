e700039cb69f938f95dd2f0827f949e9d2ca9911
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2024-07-26 11:09:36 -0400

    Parse command -> execute command

--- src/day8/Day8.java
+++ src/day8/Day8.java
@@ -15,6 +15,26 @@ class Day8
     private static final String SPACE = " ";
     private static final String X = "x";
 
+    private static void executeRectCommand(Screen pScreen, String[] pSplitCommand)
+    {
+        String[] splitDimensions = pSplitCommand[1].split(X);
+        int nbColumns = Integer.parseInt(splitDimensions[0]);
+        int nbRows = Integer.parseInt(splitDimensions[1]);
+        pScreen.turnRectangleOn(nbColumns, nbRows);
+    }
+
+    private static void executeRotateCommand(Screen pScreen, String[] pSplitCommand)
+    {
+        int index = Integer.parseInt(pSplitCommand[2].substring(2));
+        int shift = Integer.parseInt(pSplitCommand[4]);
+
+        switch (pSplitCommand[1])
+        {
+            case COLUMN -> pScreen.rotateColumn(index, shift);
+            case ROW -> pScreen.rotateRow(index, shift);
+        }
+    }
+
     public static void main(String[] pArgs) throws IOException
     {
         String inputPath = pArgs[0];
@@ -29,32 +49,12 @@ class Day8
             String[] splitCommand = inputLine.split(SPACE);
             switch (splitCommand[0])
             {
-                case CMD_RECT -> parseRectCommand(screen, splitCommand);
-                case CMD_ROTATE -> parseRotateCommand(screen, splitCommand);
+                case CMD_RECT -> executeRectCommand(screen, splitCommand);
+                case CMD_ROTATE -> executeRotateCommand(screen, splitCommand);
             }
         }
 
         System.out.println("Puzzle 1: " + screen.countPixelsOn());
-    }
-
-    private static void parseRectCommand(Screen pScreen, String[] pSplitCommand)
-    {
-        String[] splitDimensions = pSplitCommand[1].split(X);
-        int nbColumns = Integer.parseInt(splitDimensions[0]);
-        int nbRows = Integer.parseInt(splitDimensions[1]);
-        pScreen.turnRectangleOn(nbColumns, nbRows);
-    }
-
-    private static void parseRotateCommand(Screen pScreen, String[] pSplitCommand)
-    {
-        int index = Integer.parseInt(pSplitCommand[2].substring(2));
-        int shift = Integer.parseInt(pSplitCommand[4]);
-
-        switch (pSplitCommand[1])
-        {
-            case COLUMN -> pScreen.rotateColumn(index, shift);
-            case ROW -> pScreen.rotateRow(index, shift);
-        }
     }
 
     private static void printCommandAndResult(String pCommand, Screen pScreen)
