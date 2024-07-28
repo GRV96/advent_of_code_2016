7261b8312f534425b62f6e528be944d42feaa885
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2024-07-28 10:12:46 -0400

    First marker correctly interpreted

--- src/day9/Day9.java
+++ src/day9/Day9.java
@@ -14,10 +14,48 @@ class Day9
         BufferedReader reader = new BufferedReader(new FileReader(inputPath));
         String compressedContent = reader.readLine();
 
-        Pattern markerPattern = Pattern.compile("\\(\\d+x\\d+\\)");
+        Pattern markerPattern = Pattern.compile("(\\(\\d+x\\d+\\))");
         Matcher markerMatcher = markerPattern.matcher(compressedContent);
         markerMatcher.find();
 
-        System.out.println("Matches were found: " + markerMatcher.hasMatch());
+        StringBuilder decompressedStrBuilder = new StringBuilder();
+        int decompressionIndex = 0;
+        int nbGroups = markerMatcher.groupCount();
+        for (int groupIndex=0; groupIndex<nbGroups; groupIndex++)
+        {
+            int groupStart = markerMatcher.start(groupIndex);
+            int groupEnd = markerMatcher.end(groupIndex);
+
+            CompressionMarker compressionMarker = makeCompressionMarker(
+                    compressedContent, groupStart, groupEnd);
+            int sequenceStart = groupStart + groupEnd - 1;
+            int sequenceEnd = sequenceStart + compressionMarker.sequenceLength;
+
+            if (groupStart > decompressionIndex)
+            {
+                String nonCompressedSequence =
+                        compressedContent.substring(decompressionIndex, groupStart);
+                decompressedStrBuilder.append(nonCompressedSequence);
+            }
+
+            String sequence = compressedContent.substring(sequenceStart, sequenceEnd);
+            decompressedStrBuilder.append(sequence.repeat(compressionMarker.nbRepetitions));
+
+            decompressionIndex = sequenceEnd;
+        }
+
+        String decompressedContent = decompressedStrBuilder.toString();
+        System.out.println("Puzzle 1: " + decompressedContent.length());
+    }
+
+    private static CompressionMarker makeCompressionMarker(
+            String pCompressedContent, int pMatchGroupStart, int pMatchGroupEnd)
+    {
+        int xIndex = pCompressedContent.indexOf('x', pMatchGroupStart);
+        int sequenceLength = Integer.parseInt(
+                pCompressedContent.substring(pMatchGroupStart+1, xIndex));
+        int nbRepetitions = Integer.parseInt(
+                pCompressedContent.substring(xIndex+1, pMatchGroupEnd-1));
+        return new CompressionMarker(sequenceLength, nbRepetitions);
     }
 }
