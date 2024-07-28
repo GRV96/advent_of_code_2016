a13d90749fca8dd5953bdd603cc34bd77868cad8
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2024-07-27 23:16:19 -0400

    Pattern and matcher

--- src/day9/Day9.java
+++ src/day9/Day9.java
@@ -3,14 +3,21 @@ package src.day9;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class Day9
+class Day9
 {
     public static void main(String[] pArgs) throws IOException
     {
         String inputPath = pArgs[0];
         BufferedReader reader = new BufferedReader(new FileReader(inputPath));
         String compressedContent = reader.readLine();
-        System.out.println("Hello, world!\n" + compressedContent);
+
+        Pattern markerPattern = Pattern.compile("\\(\\d+x\\d+\\)");
+        Matcher markerMatcher = markerPattern.matcher(compressedContent);
+        markerMatcher.find();
+
+        System.out.println("Matches were found: " + markerMatcher.hasMatch());
     }
 }
